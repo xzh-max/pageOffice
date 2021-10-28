0f916adb137d9b859d04a5a991173c3d8cf1706e
李联敏 <2020535959@qq.com>
2021-10-28 19:02:39 +0800

    contrat esignPdf校验

--- src/main/java/com/pageOfficeServer/web/DownloadController.java
+++ src/main/java/com/pageOfficeServer/web/DownloadController.java
@@ -136,7 +136,7 @@ public class DownloadController  extends HttpServlet {
         BufferedOutputStream bos = null;
 
         String ctxPath;
-        if(!"esignPdf".equals(type) || !"contract".equals(type) && StringUtils.isEmpty(mouth)){
+        if((!"esignPdf".equals(type) || !"contract".equals(type)) && StringUtils.isEmpty(mouth)){
             ctxPath = request.getSession().getServletContext().getRealPath("/")+type+"/";
         }else{
             ctxPath = request.getSession().getServletContext().getRealPath("/")+type+"/"+mouth+"/";
